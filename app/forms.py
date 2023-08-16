e0f7d085ac54c0665078749a51de2661ae2f76f4
Johnatan Medeiros <140852210+JohnatanCsta@users.noreply.github.com>
2023-08-16 17:16:04 -0300

    a

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, TextAreaField, SubmitField
+from wtforms import StringField, EmailField, TextAreaField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 from flask_wtf.csrf import CSRFProtect
 
@@ -9,3 +9,10 @@ class Contato(FlaskForm):
     email = EmailField('email', validators=[DataRequired()])
     conteudo = TextAreaField('conteudo')
     enviar = SubmitField('enviar')
+
+class Cadastro(FlaskForm):
+    nome = StringField('nome', validators=[DataRequired()])
+    sobrenome = StringField('sobrenome', validators=[DataRequired()])
+    email = EmailField('email',validators=[DataRequired()])
+    senha = PasswordField('senha',validators=[DataRequired()]) 
+    enviar = SubmitField('Enviar')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from app import app, db
-from flask import render_template,url_for, flash, redirect, request
-from app.forms import Contato
+from flask import render_template,url_for, flash, redirect, request, session
+from app.forms import Contato, Cadastro
 import time
-from app.models import ContatoModels
+from app.models import ContatoModels, CadastroModel
 
 @app.route('/')
 def index():
@@ -14,7 +14,6 @@ def sobre():
 
 @app.route('/contato',methods=['GET','POST'])
 def contato():
-    dados_formulario = None
     formulario = Contato()
     if formulario.validate_on_submit():
         flash('Seu formulário foi enviado com sucesso!!')
@@ -25,16 +24,45 @@ def contato():
         db.session.add(novo_contato)
         db.session.commit()
 
-    return render_template('contato.html', title = 'Contato', formulario = formulario, dados_formulario = dados_formulario)
+    return render_template('contato.html', title = 'Contato', formulario = formulario)
 
 @app.route('/projetos')
 def projeto():
     return render_template('projeto.html', title ='Projetos') 
 
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['POST', 'GET'])
 def cadastro():
-    return render_template('cadastro.html', title = 'Cadastro')
+    cadastro = Cadastro()
+    if cadastro.validate_on_submit():
+        try:
+            nome = cadastro.nome.data
+            sobrenome = cadastro.sobrenome.data
+            email = cadastro.email.data
+            senha = cadastro.senha.data
 
-@app.route('/login')
+            novo_cadastro = CadastroModel(nome = nome, sobrenome = sobrenome, email=email, senha=senha)
+            db.session.add(novo_cadastro)
+            db.session.commit()
+            flash('Seu cadastro foi realizado com sucesso!')
+        except Exception as e:
+            flash('Ocorreu um erro ao cadastrar! Entre em contato com o suporte: adm@admin.com')
+            print(str(e))
+
+    return render_template('cadastro.html', title = 'Cadastro', cadastro = cadastro)
+
+@app.route('/login', methods= ['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        user = CadastroModel.query.filter_by(email = email, senha = senha).first()
+        if user and user.senha == senha:
+            session['email']  = user.id
+            flash('Seja bem vindo')
+            time.sleep(2)
+            return redirect(url_for('index'))
+        else:
+            flash('Senha ou e-mail incorreto!')
+
+
     return render_template('login.html', title = 'Login')
